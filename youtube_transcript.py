b7b74e36dadea23bdbf66bcc16ffe0f17f39bb1e
stephrex <stephrex602@gmail.com>
2025-04-25 16:23:41 +0100

    kindoff_initial

---
+++ youtube_transcript.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, VideoUnavailable
+import re
+import os
+
+app = Flask(__name__)
+
+
+def extract_video_id(url):
+    match = re.search(r"(?:v=|youtu\.be/)([A-Za-z0-9_-]{11})", url)
+    return match.group(1) if match else None
+
+
+@app.route('/transcript', methods=['POST'])
+def get_transcript():
+    import requests
+
+    url = "https://www.searchapi.io/api/v1/search"
+    params = {
+        "engine": "youtube_transcripts",
+        "video_id": "0e3GPea1Tyg"
+    }
+
+    response = requests.get(url, params=params)
+    print(response.text)
+
+
+port = int(os.environ.get('PORT', 5000))  # Default to 5000 locally
+app.run(host='0.0.0.0', port=port)
+
+
+#     url = request.json.get('url')
+#     video_id = extract_video_id(url)
+
+#     if not video_id:
+#         return jsonify({'error': 'Invalid YouTube URL'}), 400
+
+#     ytt_api = YouTubeTranscriptApi(
+#         proxy_config=GenericProxyConfig(
+#             http_url="http://50.223.246.237:80",
+#             https_url="https://170.106.136.15:13001",
+#         )
+#     )
+#     try:
+#         transcript = ytt_api.get_transcript(video_id)
+#         return jsonify({'transcript': transcript})
+#     except TranscriptsDisabled:
+#         return jsonify({'error': 'Transcripts are disabled for this video'}), 403
+#     except VideoUnavailable:
+#         return jsonify({'error': 'Video unavailable'}), 404
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
+
+# if
